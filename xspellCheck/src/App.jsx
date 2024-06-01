7bbf652c00e2901d31870d11c1a5bcead7e64f22
bijlwan07ankit <bijlwan07ankit@gmail.com>
2024-06-01 23:15:03 +0530

    added frontend

--- xspellCheck/src/App.jsx
+++ xspellCheck/src/App.jsx
@@ -1,16 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputText, setInputText] = useState("");
+  const [suggestedText, setSuggestedText] = useState("");
+const customDictionary ={
+  teh:"the",
+  wrok:"work",
+  fot:"for",
+  exampl:"example"
+}
+
+function handlechange(e){
+  const text=e.target.value;
+  setInputText(text);
+  //impliment a basic spelling check 
+
+}
 
   return (
     <>
-     xspellCkeck
+    <h1>Spell Check And Auto-Correction </h1>
+    <textArea
+    value=" "
+    onChnage={handlechange}
+    placeholder="Enter Text..."
+    row={5}
+    cols={40}
+   
+    />
+    {suggestedText &&(
+      <p>
+        Did you mean :<strong>(suggestedText)</strong>?
+      </p>
+    )}
     </>
   )
 }
+
 
 export default App
